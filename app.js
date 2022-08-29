6be78e2c55fba4b951d0952d8f2c492a1f91f597
julietangarcia <julietita.garcia@hotmail.com>
2022-08-29 20:40:16 -0300

    hasta fomr

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express();
 const path = require('path');
 app.use(express.static("public"));
 
-app.listen(3000);
-
-app.get("/",(req, res)=> {
+app.listen(3000, () => {
+    console.log("Servidor corriendo")
+});
+app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
-})
+});
+app.get("/register", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/register.html")) 
+});
+app.get("/login", (req,res)=>{         
+    res.sendFile(path.join(__dirname, "/views/login.html")) 
+});
